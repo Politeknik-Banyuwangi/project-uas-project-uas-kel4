c54e4407cfdf523f663423ede636cfd46db79ac3
FlutLab.io <>
2022-06-16 00:45:56 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,17 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Bills(),
+    );
+  }
+}
+
 class Bills extends StatefulWidget {
   @override
   _Bills createState() => _Bills();
