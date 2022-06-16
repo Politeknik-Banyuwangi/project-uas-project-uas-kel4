4fe54b8dc09df1aeec153fd26299dfe157e97fe7
FlutLab.io <>
2022-06-16 00:44:36 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,100 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Bills extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+  _Bills createState() => _Bills();
+}
+
+class _Bills extends State<Bills> {
+  int _selectedNavbar = 0;
+  var _pages = <Widget>[
+    Icon(Icons.work, size: 25, color: Colors.blue),
+    Icon(Icons.shopping_cart, size: 25, color: Colors.blue),
+    Icon(Icons.wifi, size: 25, color: Colors.blue),
+    Icon(Icons.shopping_bag_sharp, size: 25, color: Colors.blue),
+    Icon(Icons.shopping_cart, size: 25, color: Colors.blue),
+    Icon(Icons.work, size: 25, color: Colors.blue),
+    Icon(Icons.shopping_cart, size: 25, color: Colors.blue),
+    Icon(Icons.wifi, size: 25, color: Colors.blue),
+    Icon(Icons.shopping_bag_sharp, size: 25, color: Colors.blue),
+    Icon(Icons.shopping_cart, size: 25, color: Colors.blue),
+  ];
+
+  var _titleBills = [
+    'Market bills',
+    'Supermarket bills',
+    'Store bills',
+    'Wifi bills',
+    'Supermarket bills',
+    'Market bills',
+    'Supermarket bills',
+    'Store bills',
+    'Wifi bills',
+    'Supermarket bills',
+  ];
+
+  void _changeSelectedNavBar(int index) {
+    setState(() {
+      _selectedNavbar = index;
+    });
   }
-}
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Upcomming bill',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              Text('See All',
+                  style: TextStyle(
+                    color: Colors.purple,
+                    fontSize: 20.0,
+                  )),
+            ],
+          ),
+          // Container(
+          //   height: 200,
+          //   color: Colors.red,
+          // )
+          //
+          Container(
+            height: 500,
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: Card(
+                    child: ListTile(
+                      title: Text(
+                        '${_titleBills[index]}',
+                        style: TextStyle(color: Colors.black, fontSize: 24.0),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      subtitle: Text(
+                        'Desember 28, 2021',
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      leading: Container(
+                        padding: EdgeInsets.all(5),
+                        child: _pages[index],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            color: Colors.white,
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -41,198 +113,11 @@ class _HomePageState extends State<HomePage> {
             label: ('Profil'),
           ),
         ],
+        currentIndex: _selectedNavbar,
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.grey,
         showUnselectedLabels: true,
-      ),
-      backgroundColor: Colors.white,
-      body: SafeArea(
-          child: Column(
-        children: [
-          //appbar
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('E-learning',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    )),
-                Container(
-                  margin: EdgeInsets.all(20),
-                  padding: EdgeInsets.all(4),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(Icons.account_circle, size: 40),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 25),
-      )),
-    );
-  }
-}
-
-class MyCard extends StatelessWidget {
-  final double balance;
-  final int cardNumber;
-  final int month;
-  final int year;
-  final NetworkImage image;
-  const MyCard({
-    Key? key,
-    required this.balance,
-    required this.cardNumber,
-    required this.month,
-    required this.year,
-    required this.image,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-        child: Container(
-            width: 300,
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: image,
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 10),
-                Text('Balance',
-                    style: TextStyle(
-                      color: Colors.white,
-                    )),
-                SizedBox(height: 20),
-                Text('\$' + balance.toString(),
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 26,
-                    )),
-                SizedBox(height: 30),
-                Text('Account Number',
-                    style: TextStyle(
-                      color: Colors.white,
-                    )),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(cardNumber.toString(),
-                        style: TextStyle(
-                          color: Colors.white,
-                        )),
-                    Text(month.toString() + '/' + year.toString(),
-                        style: TextStyle(
-                          color: Colors.white,
-                        )),
-                  ],
-                )
-              ],
-            )));
-  }
-}
-
-class Bills extends StatefulWidget {
-  @override
-  _Bills createState() => _Bills();
-}
-
-class _Bills extends State<Bills> {
-  int _selectedNavbar = 0;
-  var _pages = <Widget>[
-    Icon(Icons.book, size: 20, color: Colors.blue),
-    Icon(Icons.book, size: 20, color: Colors.blue),
-    Icon(Icons.book, size: 20, color: Colors.blue),
-    Icon(Icons.book, size: 20, color: Colors.blue),
-    Icon(Icons.book, size: 20, color: Colors.blue),
-    Icon(Icons.book, size: 20, color: Colors.blue),
-  ];
-
-  var _titleBills = [
-    'Biologi',
-    'Kimia',
-    'Matematika',
-    'Bahasa Inggris',
-    'Bahasa Indonesia',
-    'Fisika',
-  ];
-  var _subtitleGuru = [
-    'Adi Setya Purwanto',
-    'Nur Shinta Ramadanti',
-    'Eko Saputra',
-    'Vinzen Qiy',
-    'Cintra Puspita',
-    'Azizah Saputri',
-  ];
-
-  void _changeSelectedNavBar(int index) {
-    setState(() {
-      _selectedNavbar = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Mata Pelajaran',
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          // Container(
-          //   height: 200,
-          //   color: Colors.red,
-          // )
-          //
-          Container(
-            height: 200,
-            child: ListView.builder(
-              itemCount: 6,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  child: Card(
-                    child: ListTile(
-                      title: Text(
-                        '${_titleBills[index]}',
-                        style: TextStyle(color: Colors.black, fontSize: 16.0),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      subtitle: Text(
-                        '${_subtitleGuru[index]}',
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      leading: Container(
-                        padding: EdgeInsets.all(5),
-                        child: _pages[index],
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-            color: Colors.white,
-          ),
-        ],
+        onTap: _changeSelectedNavBar,
       ),
     );
   }
