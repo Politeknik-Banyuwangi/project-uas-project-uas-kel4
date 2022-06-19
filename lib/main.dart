b7a77f42b220e9962f1cbe401c9a5cfee0e13447
FlutLab.io <>
2022-06-19 02:48:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -80,20 +80,8 @@ class _Bills extends State<Bills> {
               itemCount: 6,
               itemBuilder: (BuildContext context, int index) {
                 return Container(
-                  margin: EdgeInsets.only(left: 30, top: 100, right: 30, bottom: 50),
-                  height: double.infinity,
-                  width: double.infinity,
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10), bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
+                    color: Colors.purple.shade100,
                   ),
                   child: Card(
                     child: ListTile(
