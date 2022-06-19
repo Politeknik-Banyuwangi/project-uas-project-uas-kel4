411169ae233837d026bc881cfe60a14ec38fda6d
FlutLab.io <>
2022-06-19 01:54:46 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
                         Padding(
                           padding: const EdgeInsets.only(top: 8.0),
                           child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment,
                             children: <Widget>[
                               Icon(
                                 Icons.location_on,
