6577622cb1664a2b431a39c07ba43195e48f6068
FlutLab.io <>
2022-06-19 01:50:21 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
                                 size: 17,
                               ),
                               Padding(
-                                  padding: const EdgeInsets.only(left: 4.0),
+                                  padding: const EdgeInsets.only(left: 8.0),
                                   child: Text(
                                     'Politeknik Negeri Banyuwangi ',
                                     style: TextStyle(color: Colors.black, fontSize: 18),
