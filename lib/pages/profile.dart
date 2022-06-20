c2546ae65d387bd5c997abe3beaedebd58f56dec
FlutLab.io <>
2022-06-20 08:17:13 +0000

    Autosave

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -60,7 +60,7 @@ class ProfilePages extends StatelessWidget {
           //biodata
           Container(
             padding: EdgeInsets.fromLTRB(4.0, 0, 4.0, 3.0),
-            margin: EdgeInsets.fromLTRB(5.0, 2.0, 5.0, 4.0),
+            margin: EdgeInsets.fromLTRB(5.0, 20.0, 5.0, 4.0),
             height: 400,
             width: double.infinity,
             child: Card(
