308bab48ce513c2a3eee27bfe5b28b2c84058458
FlutLab.io <>
2022-06-20 07:00:54 +0000

    Autosave

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -61,7 +61,7 @@ class _Bills extends State<Bills> {
           // )
           //
           Container(
-              height: 500,
+              height: 550,
               child: ListView.builder(
                 itemCount: 6,
                 itemBuilder: (BuildContext context, int index) {
