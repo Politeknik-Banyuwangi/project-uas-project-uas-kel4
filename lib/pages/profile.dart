b0091e91bfa1883f72a7f5c3eff7e39d5d1307ea
FlutLab.io <>
2022-06-20 06:48:38 +0000

    Autosave

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -63,7 +63,7 @@ class ProfilePages extends StatelessWidget {
               child: Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.purple[200],
+                    color: Color(0xFFB2DEF7),
                   ),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
