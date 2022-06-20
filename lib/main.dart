aaa8aeb818f9ee4b0fed5d9c60951395fa368030
FlutLab.io <>
2022-06-20 04:59:39 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Profile();
+    );
   }
 }
