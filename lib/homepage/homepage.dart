2058ec190b349035ae9e6c2743c5d81745683e21
FlutLab.io <>
2022-06-20 08:55:26 +0000

    homepage

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -41,40 +41,82 @@ class _HomeList extends State<HomeList> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 600,
-        child: ListView.builder(
-          itemCount: 6,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
+    return Scaffold(
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Mata Pelajaran',
+                style: TextStyle(fontSize: 25.0, fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
               ),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  color: Colors.white,
-                ),
-                child: ListTile(
-                  title: Text(
-                    '${_titleBills[index]}',
-                    style: TextStyle(color: Colors.black, fontSize: 20.0),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  subtitle: Text(
-                    '${_subtitleGuru[index]}',
-                    style: TextStyle(color: Colors.black, fontSize: 15.0),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  leading: Container(
-                    padding: EdgeInsets.all(5),
-                    child: _pages[index],
-                  ),
-                ),
+            ],
+          ),
+          // Container(
+          //   height: 200,
+          //   color: Colors.red,
+          // )
+          //
+          Container(
+              height: 600,
+              child: ListView.builder(
+                itemCount: 6,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        color: Colors.white,
+                      ),
+                      child: ListTile(
+                        title: Text(
+                          '${_titleBills[index]}',
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        subtitle: Text(
+                          '${_subtitleGuru[index]}',
+                          style: TextStyle(color: Colors.black, fontSize: 15.0),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        leading: Container(
+                          padding: EdgeInsets.all(5),
+                          child: _pages[index],
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
-            );
-          },
-        ),
-        color: Colors.blue[100]);
+              color: Colors.blue[100]),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.red.shade700,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: ('Profil'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: ('Dasboard'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: ('Log out'),
+          ),
+        ],
+        currentIndex: _selectedNavbar,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        onTap: _changeSelectedNavBar,
+      ),
+    );
   }
 }
