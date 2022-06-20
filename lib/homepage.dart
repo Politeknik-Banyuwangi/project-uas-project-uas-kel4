b7154ce93a2cb5b8c34272fa403a4a9d93683b5a
FlutLab.io <>
2022-06-20 09:02:45 +0000

    Autosave

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:projectuas/widgets/homepagelist.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -87,75 +88,12 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
-          )
+          ),
+          Expanded(
+            child: HomeList(),
+          ),
         ],
       )),
     );
-  }
-}
-
-class MyCard extends StatelessWidget {
-  final double balance;
-  final int cardNumber;
-  final int month;
-  final int year;
-  final NetworkImage image;
-  const MyCard({
-    Key? key,
-    required this.balance,
-    required this.cardNumber,
-    required this.month,
-    required this.year,
-    required this.image,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-        child: Container(
-            width: 300,
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: image,
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 10),
-                Text('Balance',
-                    style: TextStyle(
-                      color: Colors.white,
-                    )),
-                SizedBox(height: 20),
-                Text('\$' + balance.toString(),
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 26,
-                    )),
-                SizedBox(height: 30),
-                Text('Account Number',
-                    style: TextStyle(
-                      color: Colors.white,
-                    )),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(cardNumber.toString(),
-                        style: TextStyle(
-                          color: Colors.white,
-                        )),
-                    Text(month.toString() + '/' + year.toString(),
-                        style: TextStyle(
-                          color: Colors.white,
-                        )),
-                  ],
-                )
-              ],
-            )));
   }
 }
---
+++ lib/widgets/homepagelist.dart
@@ -0,0 +1,80 @@
+import "package:flutter/material.dart";
+
+class HomeList extends StatefulWidget {
+  @override
+  _HomeList createState() => _HomeList();
+}
+
+class _HomeList extends State<HomeList> {
+  int _selectedNavbar = 0;
+  var _pages = <Widget>[
+    Icon(Icons.book, size: 25, color: Colors.black),
+    Icon(Icons.book, size: 25, color: Colors.black),
+    Icon(Icons.book, size: 25, color: Colors.black),
+    Icon(Icons.book, size: 25, color: Colors.black),
+    Icon(Icons.book, size: 25, color: Colors.black),
+    Icon(Icons.book, size: 25, color: Colors.black),
+  ];
+
+  var _titleBills = [
+    'Administrasi Server',
+    'Grafika Komputer',
+    'Pemrograman Web',
+    'Pengembangan Aplikasi Mobile',
+    'Jaringan Komputer Lanjut',
+    'Kewirausahaan Teknologi',
+  ];
+  var _subtitleGuru = [
+    'Farizqi Panduardi',
+    'Ruth Ema Febrita',
+    'Devit Suwardiyanto',
+    'Sepyan Purnama Kristanto',
+    'Vivien Arief Wardhany',
+    'Indira Nuansa',
+  ];
+
+  void _changeSelectedNavBar(int index) {
+    setState(() {
+      _selectedNavbar = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 600,
+        child: ListView.builder(
+          itemCount: 6,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.white,
+                ),
+                child: ListTile(
+                  title: Text(
+                    '${_titleBills[index]}',
+                    style: TextStyle(color: Colors.black, fontSize: 20.0),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: Text(
+                    '${_subtitleGuru[index]}',
+                    style: TextStyle(color: Colors.black, fontSize: 15.0),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  leading: Container(
+                    padding: EdgeInsets.all(5),
+                    child: _pages[index],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+        color: Colors.blue[100]);
+  }
+}
